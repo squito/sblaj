912cfa0741787fcbb760541facb130561ea3de4f
Imran Rashid <imran@quantifind.com>
2012-08-13 17:31:25 -0700

    real test on multinomial naive bayes classPosteriors

--- examples/src/main/scala/org/sblaj/examples/naivebayes/MultinomialNaiveBayes.scala
+++ examples/src/main/scala/org/sblaj/examples/naivebayes/MultinomialNaiveBayes.scala
@@ -1,10 +1,27 @@
 package org.sblaj.examples.naivebayes
 
+import org.sblaj.SparseBinaryVector
+import org.sblaj.ArrayUtils._
+
 /**
  *
  */
+class MultinomialNaiveBayes(val nFeatures: Int, val nClasses: Int, val classPriors: Array[Float]) {
+  val logTheta: Array[Array[Float]] = new Array[Array[Float]](nFeatures)
+  var idx = 0
+  while (idx < logTheta.length) {
+    logTheta(idx) = new Array[Float](nClasses)
+  }
+}
 
-class MultinomialNaiveBayes {
+object MultinomialNaiveBayes {
+  def classPosteriors(data: SparseBinaryVector, theta: Array[Array[Float]],
+                      logPriors: Array[Float], posteriors: Array[Float], posteriorPos: Int) {
+    Array.copy(logPriors, 0, posteriors, posteriorPos, logPriors.length)
+    data.mult(theta, posteriors) //TODO include posteriorPos
+    stableLogNormalize(posteriors, 0, logPriors.length)
+  }
+
 
 }
 
--- examples/src/test/scala/org/sblaj/examples/naivebayes/MultinomialNaiveBayesTest.scala
+++ examples/src/test/scala/org/sblaj/examples/naivebayes/MultinomialNaiveBayesTest.scala
@@ -1,13 +1,17 @@
 package org.sblaj.examples.naivebayes
 
 import org.scalatest.FunSuite
+import org.sblaj.{BaseSparseBinaryVector, SparseBinaryVector}
+import org.scalatest.matchers.ShouldMatchers
+import org.sblaj.ArrayUtils._
 
 /**
  *
  */
 
-class MultinomialNaiveBayesTest extends FunSuite {
+class MultinomialNaiveBayesTest extends FunSuite with ShouldMatchers {
   test("class posterior") {
+    //come up with rates
     val nClasses = 3
     val nDims = 20
     val theta = new Array[Array[Float]](nDims)
@@ -17,17 +21,23 @@ class MultinomialNaiveBayesTest extends FunSuite {
       for (klass <- (0 until nClasses)) {
         ratePerClass(klass) = math.random.asInstanceOf[Float]
         if (feature % nClasses == klass) {
-          ratePerClass(klass) *= 20   //"give away" features for each class
+          ratePerClass(klass) = 500   //"give away" features for each class
         }
       }
       theta(feature) = ratePerClass
     }
     normalizeColumns(theta)
-    println(matrixString(theta))
+    val logTheta = theta.map(_.map(math.log(_).asInstanceOf[Float]))
 
+    //generate some data
+    val vector : SparseBinaryVector = new BaseSparseBinaryVector(Array[Int](0,3,6), startIdx = 0, endIdx = 3)
+    val logPriors = Array[Float](0.3f, 0.3f, 0.4f).map(math.log(_).asInstanceOf[Float])
 
+    val posterior = new Array[Float](3)
+    MultinomialNaiveBayes.classPosteriors(vector, logTheta, logPriors, posterior, 0)
 
-
+    arraySum(posterior, 0, 3) should be (1.0f plusOrMinus 0.00001f)
+    posterior(0) should be (1.0f plusOrMinus 0.00001f)
   }
 
   //TODO move to more general location
@@ -48,8 +58,4 @@ class MultinomialNaiveBayesTest extends FunSuite {
     }
   }
 
-
-  def matrixString(theta: Array[Array[Float]]) : String = {
-    theta.map { _.mkString("[", ",", "]")}.mkString("\n")
-  }
 }
