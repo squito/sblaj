8630bb8d83edee690401d9a43dcc78ebac15ca46
Imran Rashid <imran@quantifind.com>
2013-03-30 16:55:54 -0700

    test data set for lda

---
+++ ml/src/test/scala/org/sblaj/ml/lda/OnlineVBLDATest.scala
@@ -0,0 +1,90 @@
+package org.sblaj.ml.lda
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.sblaj.{SparseBinaryVectorBuilder, SparseBinaryVector}
+import collection._
+import org.sblaj.ml.samplers.MultinomialSampler
+
+/**
+ *
+ */
+
+class OnlineVBLDATest extends FunSuite with ShouldMatchers {
+
+  test("basic lda") {
+    // really easy test case, but should confirm basics of whether algo works correctly or not
+    val nTopics = 10
+    val nWords = 1000
+    val nDocs = 5
+    val nTopicsPerDocument = 3
+    val wordsPerDocument = 20
+
+    val dataset = generateEasyDataSet(nTopics, nWords, nDocs, nTopicsPerDocument, wordsPerDocument)
+    dataset.show
+  }
+
+  class LdaDataSet(
+    val topicWordProbs: Array[Array[Float]], //aka lambda
+    val documentTopics: IndexedSeq[IndexedSeq[Int]],
+    val documents: IndexedSeq[SparseBinaryVector]
+  ) {
+    def show {
+      (0 until documents.length).foreach{doc =>
+        println("**** " + doc + " : " + documentTopics(doc).mkString(","))
+        println(documents(doc).mkString(","))
+        //crude way of checking if words were realy sampled from right topics
+        println(documents(doc).map{_ / 100}.mkString(","))
+      }
+    }
+  }
+
+  def generateEasyDataSet(nTopics: Int, nWords: Int, nDocuments: Int, topicsPerDocument: Int, wordsPerDocument: Int)
+  : LdaDataSet = {
+    val cumTopicWordProbs = Array.ofDim[Float](nTopics, nWords)
+    //easy data set, words are pretty distinct in each topic
+    //first make topics
+    (0 until nTopics).foreach{topic =>
+      val l = topic * nWords.toDouble / nTopics
+      val u = (topic + 1) * nWords.toDouble / nTopics
+      (0 until nWords).foreach{word =>
+        val r = math.random.toFloat
+        cumTopicWordProbs(topic)(word) = if (word >= l && word < u) r else r / 10
+      }
+      val sum = cumTopicWordProbs(topic).sum
+      var cumsum = 0.0f
+      (0 until nWords).foreach{word =>
+        val t = cumsum
+        cumsum += cumTopicWordProbs(topic)(word) / sum
+        cumTopicWordProbs(topic)(word) = t
+      }
+    }
+    //now generate documents
+    val documentTopics = new Array[IndexedSeq[Int]](nDocuments)
+    val documents = new Array[SparseBinaryVector](nDocuments)
+    (0 until nDocuments).foreach{document =>
+      val topics = chooseTopics(nTopics, topicsPerDocument)
+      documentTopics(document) = topics
+      val wordsInDoc = mutable.Set[Int]()
+      (0 until wordsPerDocument).foreach{ w => //we might get duplicate words, thats ok
+        //1. choose topic
+        val topic = topics(math.floor(math.random * topicsPerDocument).toInt)
+        //2. choose a word for that topic
+        val word = MultinomialSampler.sampleFromCumProbs(cumTopicWordProbs(topic))
+        wordsInDoc += word
+      }
+      documents(document) = SparseBinaryVectorBuilder.featureSetToVector(wordsInDoc)
+    }
+    new LdaDataSet(cumTopicWordProbs, documentTopics, documents)
+  }
+
+  private def chooseTopics(nChoices: Int, k: Int): IndexedSeq[Int] = {
+    //super inefficient, but works
+    require(k < nChoices)
+    val r = mutable.Set[Int]()
+    while (r.size < k) {
+      r += math.floor(math.random * nChoices).toInt
+    }
+    r.toArray.sorted
+  }
+}
