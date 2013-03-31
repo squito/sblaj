b82ce44f6a3c33a4acae869fcdef4f5ff186bf73
Imran Rashid <imran@quantifind.com>
2013-03-30 18:28:36 -0700

    fail gracefully if last cumProb is < 1.0, though this is really a bad sign ...

--- ml/src/main/scala/org/sblaj/ml/samplers/MultinomialSampler.scala
+++ ml/src/main/scala/org/sblaj/ml/samplers/MultinomialSampler.scala
@@ -11,10 +11,12 @@ object MultinomialSampler {
     val p = rng.nextFloat()
     var idx = 0
     while (idx < cumProbs.length) {
-      if (p < cumProbs(idx))
+      if (p <= cumProbs(idx))
         return idx
       idx += 1
     }
-    return idx
+    //ack, this actually happens often ... this is bad
+    //throw new RuntimeException("rng = " + p + " but last elem of cumProbs = " + cumProbs.last)
+    return idx - 1
   }
 }
