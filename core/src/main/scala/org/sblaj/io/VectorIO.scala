4f3a1003c3d118b1fbf3b15d2f7ed0932c83363f
Imran Rashid <imran@quantifind.com>
2013-04-09 10:08:36 -0700

    add util to convert to int vectors while filtering features

--- core/src/main/scala/org/sblaj/io/VectorIO.scala
+++ core/src/main/scala/org/sblaj/io/VectorIO.scala
@@ -1,11 +1,13 @@
 package org.sblaj.io
 
-import org.sblaj.{SparseBinaryRowMatrix, BaseSparseBinaryVector, LongSparseBinaryVector}
+import org.sblaj.{FileLongSparseBinaryRowMatrix, SparseBinaryRowMatrix, BaseSparseBinaryVector, LongSparseBinaryVector}
 import java.io._
 import io.Source
-import org.sblaj.featurization.{FeatureEnumeration, ArrayCodeLookup, RowMatrixCounts}
+import org.sblaj.featurization.{SortEnumeration, FeatureEnumeration, ArrayCodeLookup, RowMatrixCounts}
 import it.unimi.dsi.fastutil.io.{FastBufferedOutputStream, FastBufferedInputStream}
 import org.sblaj.util.Logging
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet
+import scala.collection.JavaConverters._
 
 /**
  *
@@ -50,9 +52,11 @@ object VectorIO extends Logging {
   }
 
 
-  def convertToIntVectors(longVectors: VectorFileSet, intVectors: VectorFileSet) {
-    info("loading id enumeration")
-    val idEnum = DictionaryIO.idEnumeration(longVectors)
+  def convertToIntVectors(longVectors: VectorFileSet, intVectors: VectorFileSet, idEnumOpt: Option[FeatureEnumeration]) {
+    val idEnum = idEnumOpt.getOrElse{
+      info("loading id enumeration")
+      DictionaryIO.idEnumeration(longVectors)
+    }
     val partCounts = longVectors.toSeq.map{part =>
       val partNum = part.partNum
       val buffer = new Array[Int](idEnum.size)  //max possible, probably much bigger than needed, but should be OK
@@ -79,6 +83,35 @@ object VectorIO extends Logging {
     }
     val totalCounts = partCounts.reduce{(a,b) => RowMatrixCounts(a.nRows + b.nRows, a.nCols, a.nnz + b.nnz)}
     writeMatrixCounts(totalCounts, intVectors.getMergedDimFile)
+  }
+
+  def convertToIntVectorsWithPredicate(
+    longVectors: VectorFileSet,
+    intVectors: VectorFileSet,
+    featureMinFrac: Double,
+    pred: ((String, Long)) => Boolean
+  ) {
+    //first, find columns that have meet minimum fraction
+    val longMat = new FileLongSparseBinaryRowMatrix(longVectors)
+    val origDims = longMat.dims
+    val colSums = longMat.getColSums
+    val okCols = new LongOpenHashSet()
+    colSums.long2IntEntrySet().asScala.foreach{ entry =>
+      if (entry.getIntValue.toDouble / origDims.nRows > featureMinFrac)
+        okCols.add(entry.getLongKey)
+    }
+    info("%d columns (%2f %%) pass min frac".format(okCols.size(), okCols.size().toDouble / colSums.size()))
+
+    // **ADD** columns that match the predicate
+    DictionaryIO.matchingIds(longVectors, okCols)(pred)
+    val okColArr = okCols.toLongArray
+    java.util.Arrays.sort(okColArr)
+    info("Keeping %d columns total".format(okCols.size()))
+    val okColEnum = new SortEnumeration(okColArr)
+
+    //now that we know what columns to keep, we can convert to int vectors
+    convertToIntVectors(longVectors, intVectors, Some(okColEnum))
+    remapIntDictionary(longVectors, intVectors, Some(okColEnum))
   }
 
   def remapIntDictionary(longVectors: VectorFileSet, intVectors: VectorFileSet, idEnumOpt: Option[FeatureEnumeration]) {
